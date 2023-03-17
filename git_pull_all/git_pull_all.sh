4d1edb0adc7154a2efbe960aa1d2b8bce8b0474c
Alexander Bußian <alexander.bussian@dataport.de>
2023-03-17 16:55:35 +0100

    Added echo to git_pull_all script to be more verbose what path is used

--- git_pull_all/git_pull_all.sh
+++ git_pull_all/git_pull_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Add $GIT_REPOSITORIES_PATH to your environment for example by adding the following line to your .profile
 # export GIT_REPOSITORIES_PATH="\home\<user>\<repositories_folder>
+echo "pulling all git repositories in ${GIT_REPOSITORIES_PATH}"
 cd $GIT_REPOSITORIES_PATH
 find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;

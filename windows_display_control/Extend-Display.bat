37c15b5dcf641a5838de8a9a9a920549f013e071
BlexToGo <15048849+BlexToGo@users.noreply.github.com>
2023-04-16 21:51:33 +0200

    Added my existing scripts to switch the display mode via script

---
+++ windows_display_control/Extend-Display.bat
@@ -0,0 +1,2 @@
+:: This batch script uses the windows internal tool displayswitch.exe located in %windir%\System32 to switch to the "extend" display mode
+start /min cmd /c call displayswitch.exe /extend
---
+++ windows_display_control/External-Display.bat
@@ -0,0 +1,2 @@
+:: This batch script uses the windows internal tool displayswitch.exe located in %windir%\System32 to switch to the "external" display mode
+start /min cmd /c call displayswitch.exe /external
